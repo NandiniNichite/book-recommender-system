a39ec255d6f7589c70770631b2977daedf003114
FrosT2k5 <yashpatil2k5@gmail.com>
2025-03-31 12:22:41 +0530

    add /query endpoint
    Signed-off-by: FrosT2k5 <yashpatil2k5@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
-
+from find_books import query_books
 
 popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
@@ -13,6 +13,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     df_sampled = popular_df.sample(n=50)
+    df_sampled['num_ratings']
+    df_sampled["avg_rating"] = df_sampled["avg_rating"].round(2)
+
     return render_template('index.html',
                            book_name=list(df_sampled["Book-Title"].values),
                            author= list(df_sampled["Book-Author"].values),
@@ -29,7 +32,7 @@ def recommend_ui():
 def recommend():
     user_input = request.form.get('user_input')
     index = np.where(pt.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:10]
 
     data = []
     for i in similar_items:
@@ -37,12 +40,34 @@ def recommend():
         temp_df = books[books['Book-Title'] == pt.index[i[0]]]
         item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
         item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-L'].values))
 
         data.append(item)
 
-    print(data)
     return render_template('recommend.html', data=data)
 
+@app.route('/query')
+def query_ui():
+    return render_template('query.html')
+
+@app.route('/query_books', methods=['POST'])
+def query():
+    user_input = request.form.get('query')
+    books_df = query_books(user_input, 100)
+
+    queries_result_df = books_df.sample(50)
+
+    data = []
+    for _, book in queries_result_df.iterrows():
+        item = [
+            book["Book-Title"],
+            book["Book-Author"],
+            book["Image-URL-L"],
+            book["Summary"]
+        ]
+        data.append(item)
+
+    return render_template('query.html', data=data, query=user_input)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=False, processes=1)
--- find_books.py
+++ find_books.py
@@ -7,6 +7,7 @@ import torch
 
 CSV_FILE = "clean_summaries.csv"
 INDEX_FILE = "indices.pkl"
+EMBEDDINGS_FILE = "embeddings.pt"
 
 # Load CSV
 df = pd.read_csv(CSV_FILE, sep=",")
@@ -17,7 +18,7 @@ def get_detailed_instruct(query: str) -> str:
     return f'Instruct: {task}\nQuery: {query}'
 
 # Load embeddings & indices from Pickle
-embeddings = torch.load("embeddings.pt", map_location="cuda")
+embeddings = torch.load(EMBEDDINGS_FILE, map_location="cuda")
 
 with open(INDEX_FILE, "rb") as f:
     valid_indices = pickle.load(f)  # Row indices of stored embeddings
@@ -25,7 +26,7 @@ with open(INDEX_FILE, "rb") as f:
 # Load model
 model = SentenceTransformer("intfloat/multilingual-e5-large-instruct")
 
-def find_similar_books(query, top_n=100):
+def query_books(query, top_n=100):
     query_embedding = model.encode(get_detailed_instruct(query), convert_to_tensor=True, normalize_embeddings=True)
 
     # Compute cosine similarity
@@ -35,20 +36,4 @@ def find_similar_books(query, top_n=100):
     # Get top N matches
     top_indices = np.argsort(similarity_scores)[::-1][:top_n]
 
-    # Print results
-    print("\n🔍 Top Similar Books:")
-    for i, idx in enumerate(top_indices):
-        book_idx = valid_indices[idx]  # Map back to original DataFrame index
-        book = df.iloc[book_idx]
-        print(f"{i+1}. {book['Book-Title']} by {book['Book-Author']} (Similarity: {similarity_scores[idx]:.2f})")
-        print(f"   Summary: {book['Summary']}\n")  
-
     return df.iloc[valid_indices][["ISBN_x","Book-Title", "Book-Author", "Summary", "Image-URL-L","num_ratings","avg_rating","Image-From-Google", "Keywords"]].iloc[top_indices]
-
-# Example Usage
-while True:
-    query = input("Enter a query: ")
-    find_similar_books(query)
-    input()
-    import os
-    os.system("clear")
